1ed1b6efe55d23f80c448a23a4ac31e199a48564
Stefan Mark <0mark@unserver.de>
2009-11-10 00:08:09 +0100

    Stefan Mark's dwm-transperancy patch
    Adapted from:
    <URL:http://0mark.unserver.de/dwm-sprinkles/export/31/trunk/patches/dwm-transparency.diff>
    Retrieved on: Wed Nov 25 07:38:07 2009 UTC

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const double shade           = 0.6;      /* opacity of unfocussed clients */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char dmenufont[]       = "monospace:size=10";
@@ -31,9 +32,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor   opacity */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1,       -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,       -1 },
 
 #include "rules.fragment"
 };
