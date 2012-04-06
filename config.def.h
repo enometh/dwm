9e5e6dcdaaf2e6ce41926747da898e63402431e9
Jan Christoph Ebersbach <jceb@e-jc.de>
2012-04-06 08:25:40 +0200

    dwm systray implementation
    URL: http://dwm.suckless.org/patches/systray
    Implements a system tray for dwm.
    
    http://dwm.suckless.org/patches/dwm-6.0-systray.diff
    (20120406)

--- config.def.h
+++ config.def.h
@@ -3,6 +3,8 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int showsystray        = 1;        /* False means no systray */
 static const double shade           = 0.6;      /* opacity of unfocussed clients */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
