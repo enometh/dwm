ca1f3a6e266dc6de8ab5cc8f03b02356be7db9f5
Madhu <enometh@net.meer>
2016-02-13 14:54:15 +0530

    dwm.c: (focus): do not shade unfocussed windows with opacity = 1.0 or < 0 or < shade
    [250126]
    * config.mk:  -Wno-incompatible-pointer-types (gcc14)

--- config.mk
+++ config.mk
@@ -29,6 +29,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+CFLAGS	+= -Wno-incompatible-pointer-types
 LDFLAGS  = ${LIBS}
 
 # Solaris
