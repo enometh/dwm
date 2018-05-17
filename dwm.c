daca281c8646eca2ca8cecf6cc86adeb52a2245c
Madhu <enometh@net.meer>
2018-05-17 18:05:15 +0530

    dwm.c: (drawbar): don't draw the empty square when the only windows in that tag are displayed on all tags.

--- dwm.c
+++ dwm.c
@@ -1151,7 +1151,8 @@ drawbar(Monitor *m)
 	for (c = m->clients; c; c = c->next) {
 		if (ISVISIBLE(c))
 			n++;
-		occ |= c->tags;
+		if (c->tags != TAGMASK)
+			occ |= c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
