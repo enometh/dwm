506edc7ef7a22524941dbafb1435915a74400e92
Madhu <enometh@net.meer>
2019-03-19 15:44:56 +0530

    dwm-gaps-xtile-6.0.diff

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+/*static const*/ unsigned int gappx = 6;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const double shade           = 0.6;      /* opacity of unfocussed clients */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
