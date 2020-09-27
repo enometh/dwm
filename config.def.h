2b08141c8cb3da158a37b053b17ba475f738a199
Sarthak Shah <shahsarthakw@gmail.com>
2020-09-27 15:45:09 +0530

    tatami from Sarthak Shah
    https://lists.suckless.org/hackers/2009/17595.html
    
    this ought to be adapted for xtile

--- config.def.h
+++ config.def.h
@@ -58,7 +58,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
 #include "gaplessgrid.c"
-
+#include "tatami.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -66,6 +66,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 	{ "[G]",      gaplessgrid },
+	{ "|+|",      tatami },
 };
 
 #include "patch-madhu.c"
@@ -140,6 +141,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_g,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,              XK_y,      setlayout,      {.v = &layouts[4]} }, /* tatami */
 	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask|ControlMask, XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
