f110170b01a905bdbb7b411cba3f3184c4d1f63a
Madhu <enometh@net.meer>
2023-05-10 12:58:04 +0530

    raiseme: use selmon to raise window instead of calling XRaiseWindow directly

--- dwm.c
+++ dwm.c
@@ -142,6 +142,7 @@ struct Client {
 	Monitor *mon;
 	Window win;
 	double opacity;
+	int raiseme;
 };
 
 typedef struct {
@@ -911,7 +912,7 @@ clientmessage(XEvent *e)
 				const Arg a = {.ui = 1 << i};
 				view(&a);
 				focus(c);
-				if (!c->isdesktop) XRaiseWindow(dpy, c->win);
+				c->raiseme = 1;
 				restack(selmon);
 				//WARP(c);
 			}
@@ -1313,7 +1314,7 @@ focusclienttaskbar(const Arg *arg)
     for (c = selmon->clients; c; c = c->next)
 		if (ISVISIBLE(c) && --ncc < 0) {
 		focus(c);
-		if (!c->isdesktop) XRaiseWindow(dpy, c->win);
+		c->raiseme = 1;
 		restack(selmon);
 			break;
 		}
@@ -2042,8 +2043,10 @@ restack(Monitor *m)
 	drawbar(m);
 	if (!m->sel)
 		return;
-	if (m->sel->isfloating || !m->lt[m->sellt]->arrange)
+	if (m->sel->isfloating || !m->lt[m->sellt]->arrange || m->sel->raiseme) {
 		if (!m->sel->isdesktop) XRaiseWindow(dpy, m->sel->win);
+		if (m->sel->raiseme) m->sel->raiseme = 0;
+	}
 	if (m->lt[m->sellt]->arrange) {
 		wc.stack_mode = Below;
 		wc.sibling = m->barwin;
